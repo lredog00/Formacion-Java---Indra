4b6854f3d96a49132cadf04dcc3bbc17309883a9
Luis Angel Redondo <laredondo@indra.es>
2023-03-16 13:47:17 +0100

    Primer Commit desde eclipse

---
+++ FormaciónJava/src/primero/OperacionesInterfaz.java
@@ -0,0 +1,10 @@
+package primero;
+
+public interface OperacionesInterfaz {
+	void suma(int n1, int n2);
+	void resta(int n1, int n2);
+	void division(int n1, int n2);
+	void multiplicacion(int n1, int n2);
+	void menu();
+	
+}
