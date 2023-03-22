4f61bc67708723d7bab59e99f718e94d3cadf4a0
lredog00 <lredog00@estudiantes.unileon.es>
2023-03-22 13:29:11 +0100

    Create Ejercicio13_plus.java

---
+++ Ejercicio13/Ejercicio13_plus.java
@@ -0,0 +1,92 @@
+import org.json.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+public class Ejercicio13_plus {
+	public static void main(String[] args) throws IOException 
+	{
+		
+		
+		try {
+		URL url = new URL ("https://api.thecatapi.com/v1/images/search?limit=10");
+		HttpURLConnection con = (HttpURLConnection) url.openConnection();
+		con.connect();
+		int tiempoDeRepuesta = con.getResponseCode();
+		if (tiempoDeRepuesta != 200) //200 es el tiempo de respuesta correcto.
+		{
+			throw new RuntimeException("HttpResponseCode " + tiempoDeRepuesta);
+		}
+		else
+		{
+			StringBuilder informacion = new StringBuilder();
+			Scanner sc = new Scanner(url.openStream());
+			while (sc.hasNext())
+			{
+				informacion.append(sc.nextLine());
+			}
+			sc.close();	
+			
+			System.out.println(informacion);
+			String salidaAPI = informacion.toString();
+			
+			JSONArray json = new JSONArray(salidaAPI);
+			
+			int random = (int)(Math.random()*10+1);
+			
+			JSONObject jo = new JSONObject(json.get(random).toString());
+			
+			
+			String dirURL = jo.getString("url").toString();
+			
+			URL urlImg = new URL(dirURL);
+			Image image = ImageIO.read(urlImg);
+			
+			Image resizedImage = image.getScaledInstance(300, 300, Image.SCALE_SMOOTH);
+			ImageIcon imageIcon = new ImageIcon(resizedImage);
+			JFrame jFrame = new JFrame();
+			JLabel label = new JLabel(new ImageIcon(resizedImage));
+			
+			
+			
+			jFrame.setBackground(Color.blue);
+			System.out.println(jFrame.getBackground());
+			jFrame.setTitle("Gatos");
+			jFrame.setLayout(new FlowLayout());
+			jFrame.setSize(500, 500);
+			JLabel labelTexto = new JLabel("Imagen del Gato -->");
+			labelTexto.setSize(30,30);
+			
+			jFrame.add(labelTexto);
+			jFrame.add(label);
+			jFrame.setIconImage(image);
+			
+			
+			jFrame.setVisible(true);
+			jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+			
+			
+		}
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+}
+
+
