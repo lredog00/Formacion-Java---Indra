36ccb1ce0337bd5bb2d1e15861ef17481510aed7
lredog00 <lredog00@estudiantes.unileon.es>
2023-03-22 09:44:23 +0100

    Add files via upload

---
+++ Ejercicio10/APIRest.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.net.HttpURLConnection; //Para conectarse a la API
+import java.net.MalformedURLException;
+import java.net.URL; //El manejo de URLs
+import java.util.Scanner;
+import org.json.*;
+public class APIRest {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		try {
+			URL url = new URL("https://v2.jokeapi.dev/joke/Programming?lang=es&blacklistFlags=political");
+			
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.connect();
+			
+			int timeout = con.getResponseCode();
+			
+			if(timeout!=200) {
+				
+				throw new RuntimeException("HttpResponseCode "+timeout);
+				
+			}else {
+				StringBuilder informacion = new StringBuilder();
+				Scanner sc = new Scanner(url.openStream());
+				while(sc.hasNext()) {
+					informacion.append(sc.nextLine());
+				}
+				sc.close();
+				
+				System.out.println(informacion);
+				
+				
+			}
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+
+	}
+
+}
